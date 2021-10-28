36b75c0967bc9bb1c03833ce878eb9f97ad0e02b
debbymags <debbymags@gmail.com>
2021-10-28 07:01:09 +0100

    added major functionalities

---
+++ app/src/main/java/com/deb/naturalsignapp/screens/User.kt
@@ -0,0 +1,8 @@
+package com.deb.naturalsignapp.screens
+
+data class User(
+    val uid : String? = null,
+    val location : String? = null,
+    val destination : String? = null,
+    val carRide : String? = null,
+    )
